ad2a76124411a5c3a967bc63faeeb6573600e487
kauan junker <kauanjunker@gmail.com>
2025-01-12 14:38:12 -0300

    populando o banco com permissões

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Permission;
+use App\Models\Role;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $allRoles = Role::all()->keyBy('id');
+
+        $permissions = [
+            'operation-manage' => [Role::ROLE_EQUIPE_VENDAS],
+            'view-manage' => [Role::ROLE_CLIENTE]
+        ];
+
+        foreach ($permissions as $key => $roles) {
+            $permission = Permission::create(['name' => $key]);
+            foreach ($roles as $role) {
+                $allRoles[$role]->permissions()->attach($permission->id);
+            }
+        }
+    }
+}
